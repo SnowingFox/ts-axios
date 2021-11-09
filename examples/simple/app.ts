1c157e73eb342d80b154586eb227188c1d5cfec3
SnowingFox <1503401882@qq.com>
2021-11-09 11:22:04 +0800

    feat(Upload and Download interface):

--- examples/simple/app.ts
+++ examples/simple/app.ts
@@ -1,17 +1,28 @@
 import axios from '../../src/index'
 
-const url = 'http://localhost:3000/banner'
-document.cookie = 'a=b'
+const url = `http://localhost:3000/banner`
+
 const instance = axios.create({
-  xsrfCookieName: 'XSRF-TOKEN-D',
-  xsrfHeaderName: 'X-XSRF-TOKEN-D',
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
   withCredentials: true
 })
 
+instance.interceptors.request.use(config => {
+  config.onDownloadProgress = download
+
+  return config
+})
+
 instance
-  .get(url, {
-    type: 1
-  })
+  .get(url)
   .then(res => {
     console.log(res)
   })
+  .catch(err => {
+    console.log(err)
+  })
+
+function download(e: ProgressEvent) {
+  console.log(e)
+}
--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -3,6 +3,7 @@ import { parseAllHeaders } from '../helpers/headers'
 import createError from '../helpers/error'
 import { isSameOrigin } from '../helpers/url'
 import cookie from '../helpers/cookie'
+import { isFormData } from '../helpers/utils'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -16,95 +17,126 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       cancelToken,
       withCredentials,
       xsrfCookieName,
-      xsrfHeaderName
+      xsrfHeaderName,
+      onDownloadProgress,
+      onUploadProgress
     } = config
 
     const request = new XMLHttpRequest()
     request.open(method.toUpperCase(), url!, true)
 
-    if (withCredentials) {
-      request.withCredentials = withCredentials
-    }
+    processHeaders()
 
-    if ((withCredentials || isSameOrigin(url!)) && xsrfCookieName) {
-      const xsrfVal = cookie.read(xsrfCookieName)
+    addEvents()
 
-      if (xsrfVal && xsrfHeaderName) {
-        headers[xsrfHeaderName] = xsrfVal
+    processCancel()
+
+    request.send(data)
+
+    function processHeaders(): void {
+      if (withCredentials) {
+        request.withCredentials = withCredentials
       }
-    }
 
-    Object.keys(headers).forEach(name => {
-      if (
-        data === null &&
-        name.toLowerCase() === 'content-type' &&
-        method.toLowerCase() !== 'get'
-      ) {
-        delete headers[name]
-      } else {
-        request.setRequestHeader(name, headers[name])
+      if ((withCredentials || isSameOrigin(url!)) && xsrfCookieName) {
+        const xsrfVal = cookie.read(xsrfCookieName)
+
+        if (xsrfVal && xsrfHeaderName) {
+          headers[xsrfHeaderName] = xsrfVal
+        }
       }
-    })
 
-    if (timeout) {
-      request.timeout = timeout
-    }
+      /**
+       * Delete Content-Type when data type is formdata
+       * Because brower will automatic add formdata header
+       */
+      if (isFormData(data)) {
+        delete headers['Content-Type']
+      }
 
-    if (cancelToken) {
-      cancelToken.promise.then(reason => {
-        request.abort()
-        reject(reason)
+      Object.keys(headers).forEach(name => {
+        if (
+          data === null &&
+          name.toLowerCase() === 'content-type' &&
+          method.toLowerCase() !== 'get'
+        ) {
+          delete headers[name]
+        } else {
+          request.setRequestHeader(name, headers[name])
+        }
       })
     }
 
-    request.onerror = () => {
-      reject(
-        createError({
-          name: 'Network',
-          message: 'Network Error',
+    function addEvents(): void {
+      if (timeout) {
+        request.timeout = timeout
+      }
+
+      request.onreadystatechange = () => {
+        if (request.readyState !== 4) {
+          return
+        }
+
+        if (request.status === 0) {
+          return
+        }
+
+        const responeseHeaders = request.getAllResponseHeaders()
+        const responeseData =
+          responseType && responseType !== 'text' ? request.response : request.responseText
+        const response: AxiosResponse = {
+          data: responeseData,
+          status: request.status,
+          statusText: request.statusText,
+          headers: parseAllHeaders(responeseHeaders),
           config,
-          code: null,
-          request
+          request: request
+        }
+
+        handleResponse(response)
+      }
+
+      if (onUploadProgress) {
+        request.upload.onprogress = onUploadProgress
+      }
+
+      if (onDownloadProgress) {
+        request.onprogress = onDownloadProgress
+      }
+
+      request.onerror = () => {
+        reject(
+          createError({
+            name: 'Network',
+            message: 'Network Error',
+            config,
+            code: null,
+            request
+          })
+        )
+      }
+
+      request.ontimeout = () => {
+        reject(
+          createError({
+            name: 'Timeout',
+            message: `Timeout of ${timeout} exceeded`,
+            config,
+            code: 'ECONNABORTED',
+            request
+          })
+        )
+      }
+    }
+
+    function processCancel(): void {
+      if (cancelToken) {
+        cancelToken.promise.then(reason => {
+          request.abort()
+          reject(reason)
         })
-      )
-    }
-
-    request.ontimeout = () => {
-      reject(
-        createError({
-          name: 'Timeout',
-          message: `Timeout of ${timeout} exceeded`,
-          config,
-          code: 'ECONNABORTED',
-          request
-        })
-      )
-    }
-    request.onreadystatechange = () => {
-      if (request.readyState !== 4) {
-        return
       }
-
-      if (request.status === 0) {
-        return
-      }
-
-      const responeseHeaders = request.getAllResponseHeaders()
-      const responeseData =
-        responseType && responseType !== 'text' ? request.response : request.responseText
-      const response: AxiosResponse = {
-        data: responeseData,
-        status: request.status,
-        statusText: request.statusText,
-        headers: parseAllHeaders(responeseHeaders),
-        config,
-        request: request
-      }
-
-      handleResponse(response)
     }
-
-    request.send(data)
 
     function handleResponse(response: AxiosResponse) {
       if (response.status >= 200 && response.status < 300) {
